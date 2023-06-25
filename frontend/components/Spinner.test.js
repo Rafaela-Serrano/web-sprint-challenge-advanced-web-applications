36db0303eba8e1a2f98c88645ff216d5d93cd0cd
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-06-24 19:06:33 -0500

    Fix code grade test failure :     When submitting form input values rest

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -3,19 +3,20 @@ import Spinner from "./Spinner" ;
 import { render, screen } from '@testing-library/react';
 import React from "react";
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
 
-// test('renders spinner with props changes', () => {
-
-//   const {rerender} = render ( <Spinner on={false}/> ) ;
- 
-//   expect( screen.queryByText('Please wait...')).not.toBeInTheDocument()
-
-//   rerender ( <Spinner on={true}/> ) ; 
-  
-//   expect( screen.queryByText('Please wait...')).toBeInTheDocument()
-
+// test('sanity', () => {
+//   expect(true).toBe(false)
 // })
+
+test('renders spinner with props changes', () => {
+
+  const {rerender} = render ( <Spinner on={false}/> ) ;
+ 
+  expect( screen.queryByText('Please wait...')).not.toBeInTheDocument()
+
+  rerender ( <Spinner on={true}/> ) ; 
+  
+  expect( screen.queryByText('Please wait...')).toBeInTheDocument()
+
+})
 
