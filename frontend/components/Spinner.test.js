1a284284b60d5cd3ffa1a47300c419d9d9880385
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-06-26 10:26:52 -0500

    Fix test failure Spinnir.test.js needed to import the jest-dom library

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,22 +1,22 @@
 // Import the Spinner component into this file and test
 import Spinner from "./Spinner" ;
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import React from "react";
 // that it renders what it should for the different props it can take.
 
-test('sanity', () => {
-  expect(true).toBe(false)
-})
 
 test('renders spinner with props changes', () => {
 
   const {rerender} = render ( <Spinner on={false}/> ) ;
  
-  expect( screen.queryAllByTestId("spinner")).not.toBeInTheDocument("spinner")
+  expect( screen.queryByText("Please wait...")).not.toBeInTheDocument() 
 
   rerender ( <Spinner on={true}/> ) ; 
+
+  console.log(screen.queryByText("Please wait..."))
   
-  expect( screen.queryAllByTestId("spinner")).toBeInTheDocument("spinner")
+  expect( screen.queryByText("Please wait...")).toBeInTheDocument() 
 
 })
 
